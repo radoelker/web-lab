45e42cb3ec5477d651adcdd5c13a8f8d843fc4c5
Rainer Doelker <rdoelker@ironhack.com>
2026-03-30 16:49:34 +0200

    feat: flipping to darkness

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("JS is connected! 🚀");
+// Source - https://stackoverflow.com/a/76961890
+// // Posted by mplungjan
+// // Retrieved 2026-03-30, License - CC BY-SA 4.0
+//
+$(function() {
+   $("#toggle_checkbox").on("click", () => {
+       $('body').toggleClass("dark",this.checked)
+         });
+   });
